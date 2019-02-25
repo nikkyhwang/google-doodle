2b6809d747a7c76aed89b407accdfc339d1f6ca3
Nikky Hwang <nhwang@eagles.ewu.edu>
2019-02-25 00:47:25 -0800

    Codepen Copy
    and Paste

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
 // Everything OK?
-console.log('OK');
+console.log('My Tattoo Themed Google Doodle!');
 
 // Your Awesome Scripts
+$("path").hide();
+setTimeout(function(){
+	$("path").show();
+},3000);
